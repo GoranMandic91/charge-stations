fe7310cb2ace2fd2928dcbe50f74b772946af339
Goran Mandic <godzi1991@gmail.com>
2024-09-28 15:03:00 +0200

    [api] Return token as part of response instead in cookie

--- apps/api/src/routes/auth/login.ts
+++ apps/api/src/routes/auth/login.ts
@@ -24,13 +24,15 @@ const loginHandler: RequestHandler = async (req, res) => {
 
     const { _id, email, fullName, role, createdAt, updatedAt } = user;
 
-    return res
-      .cookie("Authorization", token, {
-        expires: expires,
-        httpOnly: true,
-        secure: true,
-      })
-      .json({ _id, email, fullName, role, createdAt, updatedAt });
+    return res.json({
+      _id,
+      email,
+      fullName,
+      role,
+      token,
+      createdAt,
+      updatedAt,
+    });
   }
 
   return res.status(401).json({ message: "Unauthorized" });
